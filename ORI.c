1a21c5fe922b7919cc5d2235b9c8781c0fb21c5e
mustafa0411 <mustafaalshebeeb@gmail.com>
2025-10-28 11:12:50 -0400

    authorship comments

--- ORI.c
+++ ORI.c
@@ -2,7 +2,7 @@
  * Authors: Mustafa Al-Shebeeb, Seth McBee, Finan Grmay, Bennet Melilo
  * Date: 10/28/2025
  * Byteforge Systems
- * Group: Hefeida Group #3
+ * Group: Hefeida Group #4
  */
 
 // dont get paid enough to implement
--- SLTI.c
+++ SLTI.c
@@ -2,7 +2,7 @@
  * Authors: Mustafa Al-Shebeeb, Seth McBee, Finan Grmay, Bennet Melilo
  * Date: 10/28/2025
  * Byteforge Systems
- * Group: Hefeida Group #3
+ * Group: Hefeida Group #4
  */
 
 // give this to the intern
